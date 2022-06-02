cd2bbb0df315c4ddd4d129d5f0f22887622d2193
Dorian Loret <dorian.loret20@gmail.com>
2022-06-02 19:12:09 +0200

    end

--- script.js
+++ script.js
@@ -7,3 +7,43 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY >= 100) {
+         
+         navbar.style.height = "45px";
+        
+    } else {
+        navbar.style.height = "90px"
+    }
+
+    if (window.scrollY >= 250) {
+
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = "translateX(0px)";
+    } else {
+
+        imgImprovise.style.opacity = 0;
+        imgImprovise.style.transform = "translateX(-200px)";
+
+    }
+
+    if (window.scrollY >= 1300) {
+
+        popup.style.opacity = 1;
+        popup.style.transform = "translateX(0px)";
+
+    } else {
+
+        popup.style.opacity = 0;
+        popup.style.transform = "translateX(400px)";
+    }
+
+})
+
+closeBtn.addEventListener("click", () => {
+
+    popup.remove()
+}, ) 
+
